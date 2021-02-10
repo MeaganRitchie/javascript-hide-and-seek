8e231bb1dc6b8108f0941427674d1c30f04a305d
MeaganRitchie <meaganritchie@gmail.com>
2021-02-09 20:53:06 -0700

    commit tree 8a79b451729ea0834c2b0628c60f0ba9a3370d8e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector) 
+}
 
+function nestedTarget() {
+    return document.querySelector("#nested .target")
+}
+
+function deepestChild() {
+    return document.querySelector("#grand-node div div div div")
+}
+
+function increaseRankBy(n) {
+    var numbers = document.querySelectorAll(".ranked-list li")
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].innerHTML = parseInt(numbers[i].innerHTML) + 3 
+    }
+}
